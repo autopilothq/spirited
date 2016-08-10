821e182f3d3e0f50d1be547d5f0253362b8df36e
Rolly Fordham <rolly@luma.co.nz>
2016-08-10 21:44:21 +0800

    use source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        spirited: './src/index.js',
-    },
-    output: {
-        path: "./dist",
-        filename: "[name].bundle.js",
-        library: 'spirited',
-        libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            {test: /\.json$/, exclude: /node_modules/, loader: 'json-loader'}
-        ]
-    },
+  devtool: 'source-map',
+
+  entry: {
+    spirited: './src/index.js',
+  },
+  output: {
+    path: './dist',
+    filename: '[name].bundle.js',
+    library: 'spirited',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.json$/, exclude: /node_modules/, loader: 'json-loader'},
+    ],
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+  ],
 };
