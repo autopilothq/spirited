eca4ec3b12e9eeac4bbb73a2d68cb9b32f06f0de
Rolly Fordham <rolly@luma.co.nz>
2016-09-05 13:11:00 +1000

    removing old notes

--- src/index.js
+++ src/index.js
@@ -23,51 +23,6 @@ const makePlaybackGroup = (animations, aggregationMethod, {gracefulStop} = {}) =
 };
 
 
-/*
-import { default as animatem compose, combine } from 'animate';
-const shake = animate([0], 200, { loop: true, easing: 'easeInOutQuad' })
-                  .tween([10], 400)
-                  .tween([-10]);
-
-
-const moveTo = animate([0, 0], 200, { loop: true, easing: 'easeInOutQuad' })
-                  .tween([200, 150], 400)
-                  .tween([200, 50]);
-
-// Combining animations
-// Copies the behaviour of animations but not it's state (if it had already been started)
-const shakeNMove = combine(moveTo, shake)
-                        .onTick(([x, y], shape) => {
-                          shape.bbox.minX = x;
-                          shape.bbox.minY = y;
-                        });
-
-// Composing animations
-// Copies the behaviour of animations but not it's state (if it had already been started)
-const shakeNMove = compose(moveTo, shake)
-                        .onTick((shakeOffset, movePosition, shape) => {
-                          shape.bbox.minX = x;
-                          shape.bbox.minY = y;
-                        });
-
-shapeShaker = shake(shape)
-  .onTick((value, time) => { ... })
-  .onComplete(() => { ... })
-  .start()
-
-
-shapeShakeNMove = shakeNMove(shape)
-  .onTick((value, time) => { ... })
-  .onComplete(() => { ... })
-  .start()
-
-animate.tick()
-shapeShaker.stop();
-shapeShaker.destroy();
-shapeShaker.tick(time);     // <== private API
-*/
-
-
 /**
  * [default description]
  *
